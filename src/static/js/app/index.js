c9e955fce3d1e6b27aa107d8bb23703ce4d173da
csboyty <csboyty@gmail.com>
2016-09-13 16:21:23 +0800

    编写页面

--- src/static/js/app/index.js
+++ src/static/js/app/index.js
@@ -1,15 +1,22 @@
 requirejs.config({
     baseUrl: 'static/js/lib',
     /*config:{
-        //这里面可以防止全局配置，有多种语法，详见requirejs.cn文档
-    },*/
+     //这里面可以防止全局配置，有多种语法，详见requirejs.cn文档
+     },*/
     paths: {
         app: '../app'
+    },
+    //引入没有用define定义的库
+    shim:{
+        juicer:{
+            exports: 'juicer'
+        }
     }
 });
 
 // Start the main app logic.
-requirejs(['jquery',"app/config"],
-    function   ($,config) {
+requirejs(['jquery', "juicer", "app/config"],
+    function ($, juicer, config) {
+        console.log(juicer);
         console.log(config.version);
     });
